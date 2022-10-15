68f59b4844b575d5e672ca7b9b82e8c88b4d8e31
Keer Zhao <91752854+kekike00@users.noreply.github.com>
2022-10-14 21:31:18 -0400

    Add files via upload

---
+++ components/Nav.vue
@@ -0,0 +1,21 @@
+<template>
+ <nav>
+      <NuxtLink to="/">Home</NuxtLink>
+        </nav>
+</template>
+
+<style scoped>
+nav{
+    color:yellow;
+    text-align:right;
+    padding-right:2%;
+    font-family: 'Montserrat', sans-serif;
+}
+
+a{
+    color:red;
+    cursor:grab;
+    text-decoration:line-through;
+    text-align:center;
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+       
+        <Nav /> 
+        <Nuxt />
+       <nav class="socialMedia"> <a href="https://www.instagram.com/kekike_zhao/">Instagram</a>
+        <span> | </span>
+        <a href="https://twitter.com/kekike_zhao">Twitter</a>
+       </nav>
+    </div>
+
+  </template>
+
+  <style scoped>
+  
+  .socialMedia{ 
+    text-align: right;
+    padding-right:2%;
+    font-family: 'Montserrat', sans-serif;
+   
+  }
+ 
+  a{
+    color:red;
+    cursor:grab;
+    text-decoration:line-through;
+}
+  </style>
